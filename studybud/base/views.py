784100aea9a1a1f920379d519af02ee3fe6a8a05
Kavindu Fernando <kavindu2331@gmail.com>
2023-01-12 13:29:43 +0530

    change1

--- studybud/base/views.py
+++ studybud/base/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
 # Create your views here.
+
+
+def home(request):
+    return HttpResponse('Home page')
+
+
+def room(request):
+    return HttpResponse('ROOM')
