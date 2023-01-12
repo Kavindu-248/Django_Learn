3a5a6a9e5aa22458f707e614172198a6a3b12d8d
Kavindu Fernando <kavindu2331@gmail.com>
2023-01-12 14:22:42 +0530

    studybud

--- base/views.py
+++ base/views.py
@@ -5,8 +5,8 @@ from django.http import HttpResponse
 
 
 def home(request):
-    return HttpResponse('Home page')
+    return render(request, 'home.html')
 
 
 def room(request):
-    return HttpResponse('ROOM')
+    return render(request, 'room.html')
