31aa0a90ede5a9db23f3d934c155c0c53cb899ba
Kavindu Fernando <kavindu2331@gmail.com>
2023-01-13 14:41:03 +0530

    templating

--- base/views.py
+++ base/views.py
@@ -3,9 +3,15 @@ from django.shortcuts import render
 
 # Create your views here.
 
+rooms = [
+    {'id': 1, 'name': 'Lets learn python'},
+    {'id': 2, 'name': 'Design with me'},
+    {'id': 3, 'name': 'Front-end developers'},
+],
+
 
 def home(request):
-    return render(request, 'home.html')
+    return render(request, 'home.html', {'rooms': rooms})
 
 
 def room(request):
