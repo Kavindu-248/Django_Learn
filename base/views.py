13a27bc80ea55eca7597a5fb29d14f456c9f9cae
Kavindu Fernando <kavindu2331@gmail.com>
2023-01-15 19:27:54 +0530

    templating changes

--- base/views.py
+++ base/views.py
@@ -11,7 +11,8 @@ rooms = [
 
 
 def home(request):
-    return render(request, 'home.html', {'rooms': rooms})
+    context = {'rooms': rooms}
+    return render(request, 'home.html', context)
 
 
 def room(request):
