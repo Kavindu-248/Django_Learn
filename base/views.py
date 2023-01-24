520870d8a3e526f0a93b33e5eb79550e97c69f52
Kavindu Fernando <kavindu2331@gmail.com>
2023-01-24 15:34:22 +0530

    chnages to views.py and home.html

--- base/views.py
+++ base/views.py
@@ -13,7 +13,8 @@ from .forms import RoomForm
 
 
 def home(request):
-    rooms = Room.objects.all()
+    q = request.GET.get('q') if request.GET.get('q') != None else ''
+    rooms = Room.objects.filter(topic_name_icontains=q)
 
     topics = Topic.objects.all()
 
