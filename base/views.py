86e13bba91d0bca9fac7c127fe1480676b2f25b6
Kavindu Fernando <kavindu2331@gmail.com>
2023-01-24 13:15:34 +0530

    added delete view and template

--- base/views.py
+++ base/views.py
@@ -41,5 +41,21 @@ def updateRoom(request, pk):
     room = Room.objects.get(id=pk)
     form = RoomForm(instance=room)
 
+    if request.method == 'POST':
+        form = RoomForm(request.POST, instance=room)
+        if form.is_valid():
+            form.save()
+            return redirect('home')
+
     context = {}
     return render(request, 'base/room_form.html', context)
+
+
+def deleteRoom(request, pk):
+    room = Room.objects.get(id=pk)
+    if request.method == 'POST':
+        room.delete()
+        return redirect('home')
+
+    context = {'item': room}
+    return render(request, 'base/delete.html', {'obj': room})
