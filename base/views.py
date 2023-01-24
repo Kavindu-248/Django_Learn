d19375b7e5c75470c9ebf4c0bdbcbb1f8f10ce8e
Kavindu Fernando <kavindu2331@gmail.com>
2023-01-24 09:11:35 +0530

    changes to CRUD form

--- base/views.py
+++ base/views.py
@@ -26,11 +26,13 @@ def room(request, pk):
 
 def createRoom(request):
     form = RoomForm()
+
     if request.method == 'POST':
         form = RoomForm(request.POST)
         if form.is_valid():
             form.save()
             return redirect('home')
+
     context = {'form': form}
     return render(request, 'base/room_form.html', context)
 
