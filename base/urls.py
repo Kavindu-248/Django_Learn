d29eb74a97235f6a4828bac52e575d11c12c5ba0
Kavindu Fernando <kavindu2331@gmail.com>
2023-01-26 17:15:27 +0530

    changes

--- base/urls.py
+++ base/urls.py
@@ -5,6 +5,7 @@ from .import views
 urlpatterns = [
 
     path('login/', views.loginPage, name="login"),
+    path('logout/', views.logoutUser, name="logout"),
 
 
     path('', views.home, name="home"),
--- base/views.py
+++ base/views.py
@@ -38,6 +38,11 @@ def loginPage(request):
     return render(request, 'base/login_register.html', context)
 
 
+def logoutUser(request):
+    logout(request)
+    return redirect('home')
+
+
 def home(request):
     q = request.GET.get('q') if request.GET.get('q') != None else ''
 
