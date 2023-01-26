7a5e5fe15780b4406ee7c07c90e5576ea7c75c3b
Kavindu Fernando <kavindu2331@gmail.com>
2023-01-26 10:00:17 +0530

    login feature changes

--- base/views.py
+++ base/views.py
@@ -1,7 +1,9 @@
 from django.shortcuts import render, redirect
+from django.contrib import messages
 from .models import Room, Topic
 from .forms import RoomForm
 from django.db.models import Q
+from django.contrib.auth.models import User
 
 
 # Create your views here.
@@ -17,6 +19,11 @@ def loginPage(request):
     if request.method == 'POST':
         username = request.POST.get('username')
         password = request.POST.get('password')
+
+        try:
+            user = User.objects.get(username=username)
+        except:
+            messages.error(request, 'User does not exist')
 
     context = {}
     return render(request, 'base/login_register.html', context)
