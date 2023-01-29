1c2765ac8059d0440432e881f0f87513cdfe0d47
Kavindu Fernando <kavindu2331@gmail.com>
2023-01-29 21:58:48 +0530

    views.py change

--- base/views.py
+++ base/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render, redirect
 from django.contrib import messages
+from django.http import HttpResponse
 from django.contrib.auth.decorators import login_required
 from .models import Room, Topic
 from .forms import RoomForm
@@ -80,9 +81,13 @@ def createRoom(request):
     return render(request, 'base/room_form.html', context)
 
 
+@login_required(login_url='login')
 def updateRoom(request, pk):
     room = Room.objects.get(id=pk)
     form = RoomForm(instance=room)
+
+    if request.user != room.user:
+        return HttpResponse('You are not allowed to view this page')
 
     if request.method == 'POST':
         form = RoomForm(request.POST, instance=room)
