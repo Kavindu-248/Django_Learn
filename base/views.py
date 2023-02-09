d8ae9981b421b34c1d90f80cd8af13b13afd734a
Kavindu Fernando <kavindu2331@gmail.com>
2023-02-09 11:41:41 +0530

    messages refactor

--- base/views.py
+++ base/views.py
@@ -88,7 +88,8 @@ def home(request):
 
 def room(request, pk):
     room = Room.objects.get(id=pk)
-    context = {'room': room}
+    messages = room.message_set.all()
+    context = {'room': room, 'messages': messages}
     return render(request, 'base/room.html', context)
 
 
